0dc6a47b23d942c09f296db9a63299579f42f59f
akash <akashroy7796@gmail.com>
2020-02-12 16:16:48 -0500

    onClickListener Working

--- GroupMessenger1/app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger1/GroupMessengerActivity.java
+++ GroupMessenger1/app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger1/GroupMessengerActivity.java
@@ -1,6 +1,7 @@
 package edu.buffalo.cse.cse486586.groupmessenger1;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
@@ -10,6 +11,18 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * GroupMessengerActivity is the main Activity for the assignment.
@@ -44,37 +57,23 @@ public class GroupMessengerActivity extends Activity {
          * and send it to other AVDs.
          */
 
-        findViewById(R.id.button4).setOnKeyListener(new View.OnKeyListener() {
+        findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
+            public void onClick(View v) {
+                               Toast.makeText(getApplicationContext(), "Clicked", Toast.LENGTH_SHORT).show();
                 final EditText editText = (EditText) findViewById(R.id.editText1);
-                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
-                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
-                    /*
-                     * If the key is pressed (i.e., KeyEvent.ACTION_DOWN) and it is an enter key
-                     * (i.e., KeyEvent.KEYCODE_ENTER), then we display the string. Then we create
-                     * an AsyncTask that sends the string to the remote AVD.
-                     */
+
                     String msg = editText.getText().toString() + "\n";
                     Log.d("Edit Text","Text Received"+msg);
                     editText.setText(""); // This is one way to reset the input box.
                     TextView localTextView = (TextView) findViewById(R.id.textView1);
                     localTextView.append("\t" + msg); // This is one way to display a string.
 
-                    /*
-                     * Note that the following AsyncTask uses AsyncTask.SERIAL_EXECUTOR, not
-                     * AsyncTask.THREAD_POOL_EXECUTOR as the above ServerTask does. To understand
-                     * the difference, please take a look at
-                     * http://developer.android.com/reference/android/os/AsyncTask.html
-                     */
-                       new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, msg, myPort);
-                    return true;
-                }
-                return false;
-
+                       new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, msg);
 
             }
         });
+
     }
 
     @Override
@@ -82,6 +81,100 @@ public class GroupMessengerActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_group_messenger, menu);
         return true;
+    }
+    /***
+     * ServerTask is an AsyncTask that should handle incoming messages. It is created by
+     * ServerTask.executeOnExecutor() call in SimpleMessengerActivity.
+     *
+     * Please make sure you understand how AsyncTask works by reading
+     * http://developer.android.com/reference/android/os/AsyncTask.html
+     *
+     * @author stevko
+     *
+     */
+    private class ServerTask extends AsyncTask<ServerSocket, String, Void> {
+
+        @Override
+        protected Void doInBackground(ServerSocket... sockets) {
+            ServerSocket serverSocket = sockets[0];
+
+
+
+            return null;
+        }
+
+        protected void onProgressUpdate(String...strings) {
+            /*
+             * The following code displays what is received in doInBackground().
+             */
+            String strReceived = strings[0].trim();
+            TextView remoteTextView = (TextView) findViewById(R.id.textView1);
+            remoteTextView.append(strReceived + "\t\n");
+            TextView localTextView = (TextView) findViewById(R.id.textView1);
+            localTextView.append("\n");
+
+            /*
+             * The following code creates a file in the AVD's internal storage and stores a file.
+             *
+             * For more information on file I/O on Android, please take a look at
+             * http://developer.android.com/training/basics/data-storage/files.html
+             */
+
+            String filename = "SimpleMessengerOutput";
+            String string = strReceived + "\n";
+            FileOutputStream outputStream;
+
+            try {
+                outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+                outputStream.write(string.getBytes());
+                outputStream.close();
+            } catch (Exception e) {
+                Log.e(TAG, "File write failed");
+            }
+
+            return;
+        }
+    }
+    /***
+     * ClientTask is an AsyncTask that should send a string over the network.
+     * It is created by ClientTask.executeOnExecutor() call whenever OnKeyListener.onKey() detects
+     * an enter key press event.
+     *
+     * @author stevko
+     *
+     */
+    private class ClientTask extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected Void doInBackground(String... msgs) {
+            try {
+                Socket socket=null;
+                String remotePorts[] = new String[]{"11108", "11112", "11116", "11120", "11124"};
+
+                for(String port:remotePorts)
+                {
+                    socket = new Socket(InetAddress.getByAddress(new byte[]{10, 0, 2, 2}),
+                            Integer.parseInt(port));
+                }
+
+                String msgToSend = msgs[0];
+                DataOutputStream outputStream=new DataOutputStream(socket.getOutputStream());
+                Log.d("Client","Message to be Sent"+msgToSend);
+                outputStream.writeUTF(msgToSend);
+                Log.d("Client","Message Sent");
+                Log.d("Server","Creating a dummy output stream so handshake will be complete and we can close the socket");
+                DataInputStream inputStream=new DataInputStream(socket.getInputStream());
+                String dummyString=inputStream.readUTF();
+                socket.close();
+
+            } catch (UnknownHostException e) {
+                Log.e(TAG, "ClientTask UnknownHostException");
+            } catch (IOException e) {
+                Log.e(TAG, "ClientTask socket IOException");
+            }
+
+            return null;
+        }
     }
 }
 
